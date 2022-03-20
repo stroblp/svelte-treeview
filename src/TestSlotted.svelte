5f5d54bb4794ca1d16c7d838543a396ebec91745
stroblp <stroblp@hotmail.com>
2022-03-20 11:42:19 +0000

    color slotted component

--- src/TestSlotted.svelte
+++ src/TestSlotted.svelte
@@ -1,3 +1,9 @@
 <div>
-    <input type ="checkbox"/>
+    <label>Dynamic Component</label>
+    <input type="checkbox" />
 </div>
+<style>
+    label{
+        color:rgb(25, 0, 255)
+    }
+</style>
